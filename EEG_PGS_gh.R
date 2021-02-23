08ef79d1d7cd637017bc2f6fc2bfdb34ccf9b200
annagui <35510131+annagui@users.noreply.github.com>
2021-02-23 14:26:01 +0100

    added raincloud plots and aut prediction

--- EEG_PGS_gh.R
+++ EEG_PGS_gh.R
@@ -1,13 +1,18 @@
 ### Statistical analyses for 
-# "Polygenic contributions to face-sensitive cortical responses in infants with a family history of autism"
+# "Polygenic liability for autism alters face-sensitive cortical responses from infancy"
 #author: "AnnaGui"
-#date: "29/01/2021"
+#date: "23/02/2021"
 
 ### Load packages
 library(plyr)
 library(ggplot2)
 library(car)
 library(dplyr)
+library(FSA)
+library(lmtest)
+library(lsr)
+library(rstatix)
+library(pscl)
 library(ggpubr)
 
 ### Load files
@@ -64,6 +69,11 @@ sd(ds_prs$X0.01)
 mean(ds_prs$X0.5)
 sd(ds_prs$X0.5)
 
+mean(ds_prs$M_ELC_SS_8)
+sd(ds_prs$M_ELC_SS_8)
+
+mean(ds_prs$M_ELC_SS_36, na.rm=T)
+sd(ds_prs$M_ELC_SS_36, na.rm=T)
 
 #*By group:*
 
@@ -80,8 +90,9 @@ ddply(ds_prs, "Group", summarise,  mean(M_CA_8, na.rm = T), sd(M_CA_8, na.rm = T
 ddply(ds_prs, "Group", summarise,  mean(fn_n290_lat), sd(fn_n290_lat))
 ddply(ds_prs, "Group", summarise,  mean(X0.01), sd(X0.01))
 ddply(ds_prs, "Group", summarise,  mean(X0.5), sd(X0.5))
-
-
+ddply(ds_prs, "Group", summarise,  mean(M_ELC_SS_8), sd(M_ELC_SS_8))
+ddply(ds_prs, "Group", summarise,  mean(M_ELC_SS_36, na.rm=T), sd(M_ELC_SS_36, na.rm=T))
+ddply(ds_prs, "Group", summarise,  mean(ADOS2_CSS_36, na.rm=T), sd(ADOS2_CSS_36, na.rm=T))
 
 ###ANOVAs: 
 
@@ -98,6 +109,7 @@ shapiro.test(ds_prs[ds_prs$Group=='3',]$M_CA_8)
 
 #ANOVA
 summary(aov(M_CA_8~Group, data=ds_prs))
+etaSquared(aov(M_CA_8~Group, data=ds_prs))
 
 #post-hoc
 TukeyHSD(aov(M_CA_8~Group, data=ds_prs))
@@ -114,6 +126,7 @@ shapiro.test(ds_prs[ds_prs$Group=='3',]$fn_n290_lat)
 
 #ANOVA
 summary(aov(fn_n290_lat~Group, data=ds_prs))
+etaSquared(aov(fn_n290_lat~Group, data=ds_prs))
 
 #post-hoc
 TukeyHSD(aov(fn_n290_lat~Group, data=ds_prs))
@@ -139,16 +152,44 @@ N290_byGroup<-ggplot(ds_prs_plot, aes(x=Group, y=fn_n290_lat, colour=Group, fill
   geom_bar(position =position_dodge(.9), stat = 'summary', fun = 'mean', color="black",alpha = 0.5) +
   geom_errorbar(stat = 'summary', position = position_dodge(.9), width=.2, color="black" ) +
   geom_jitter( aes(x=Group, y=fn_n290_lat, shape=Group), position=position_jitter(width=0.3, height=0.2), alpha=0.9) +
-  ylab("F-N N290 latency (ms)\n Non-face > Face                                                 Face > Non-face") +
+  ylab("F-N N290 latency (ms)\n Non-face > Face                           Face > Non-face") +
   xlab("") +
   scale_x_discrete(breaks=c("0", "1", "2","3"),labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
   scale_shape_manual(values=c("0"=1, "1"=13,"2"= 4,"3"=3), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
   scale_fill_manual(values=c("0"='springgreen4', "1"='royalblue',"2"= 'darkorange1',"3"='firebrick'), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
   scale_color_manual(values=c("0"='springgreen4', "1"='royalblue',"2"= 'darkorange1',"3"='firebrick'), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
   theme_bw() +
-  theme(axis.title.y = element_text(vjust=-1, size=10, hjust=0.5), axis.line = element_line(colour = "black"), text = element_text(size=12),  panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + guides(fill=F, color=F, shape=F)  
+  theme(axis.title.y = element_text(vjust=-1, size=12, hjust=0.5), axis.line = element_line(colour = "black"), text = element_text(size=12),  panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + guides(fill=F, color=F, shape=F)  
 print(N290_byGroup)
 #dev.off()
+
+
+
+sumN290 <- summarySE(ds_prs_plot, measurevar = "fn_n290_lat", groupvars="Group")
+
+#make sure summarySE.R and R_rainclouds.R are in the WD
+source(paste(WORKDIR,'/summarySE.R', sep=''))
+source(paste(WORKDIR,'/R_rainclouds.R', sep=''))
+
+#jpeg(file="rainbowN290_byGroup.jpeg",width = 5, height = 5, units = 'in', res = 300)
+rainbowN290_byGroup<-ggplot(ds_prs_plot, aes(x=Group,y=fn_n290_lat, fill = Group, colour = Group)) +
+  geom_flat_violin(aes(fill = Group),position = position_nudge(x = .1, y = 0), adjust = 1.5, trim = FALSE, alpha = .5, colour = NA)+
+  geom_point(aes(x = as.numeric(Group)-.15, y = fn_n290_lat, colour = Group, shape=Group),position = position_jitter(width = .05), size = 1.5)+
+  geom_boxplot(aes(x = Group, y = fn_n290_lat, fill = Group),outlier.shape = NA, alpha = .5, width = .1, colour = "black")+
+  ylab("F-N N290 latency (ms)\n Non-face > Face                           Face > Non-face") +
+  xlab("") +
+  scale_x_discrete(breaks=c("0", "1", "2","3"),labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  geom_point(data = sumN290, aes(x = as.numeric(Group)+.1, y = fn_n290_lat_mean, group = Group), shape = 18, color="black") +
+  geom_errorbar(data = sumN290, aes(x = as.numeric(Group)+.1, y = fn_n290_lat_mean, group = Group, ymin = fn_n290_lat_mean-se, ymax = fn_n290_lat_mean+se), width = .07, color="black")+
+  scale_shape_manual(values=c("0"=1, "1"=13,"2"= 4,"3"=3), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  scale_fill_manual(values=c("0"='springgreen4', "1"='royalblue',"2"= 'darkorange1',"3"='firebrick'), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  scale_color_manual(values=c("0"='springgreen4', "1"='royalblue',"2"= 'darkorange1',"3"='firebrick'), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  ggtitle("") +
+  theme_bw() +
+  theme(axis.title.y = element_text(vjust=-1, size=12, hjust=0.5), axis.line = element_line(colour = "black"), text = element_text(size=12),  panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + guides(fill=F, color=F, shape=F)  
+print(rainbowN290_byGroup)
+#dev.off()
+
 
 
 ##Autism PGS (PT=0.01) by Group
@@ -162,6 +203,7 @@ shapiro.test(ds_prs[ds_prs$Group=='3',]$X0.01)
 
 #ANOVA
 summary(aov(ds_prs$X0.01~as.factor(ds_prs$Group)))
+etaSquared(aov(X0.01~Group, data=ds_prs))
 
 #post-hoc
 TukeyHSD(aov(ds_prs$X0.01~as.factor(ds_prs$Group)))
@@ -194,6 +236,28 @@ print(ASD_byGroup)
 #dev.off()
 
 
+sumPGS_ASD <- summarySE(ds_prs_plot, measurevar = "X0.01", groupvars="Group")
+
+#jpeg(file="rainbowASD_byGroup.jpeg",width = 5, height = 5, units = 'in', res = 300)
+rainbowASD_byGroup<-ggplot(ds_prs_plot, aes(x=Group,y=X0.01, fill = Group, colour = Group)) +
+  geom_flat_violin(aes(fill = Group),position = position_nudge(x = .1, y = 0), adjust = 1.5, trim = FALSE, alpha = .5, colour = NA)+
+  geom_point(aes(x = as.numeric(Group)-.15, y = X0.01, colour = Group, shape=Group),position = position_jitter(width = .05), size = 1.5)+
+  geom_boxplot(aes(x = Group, y = X0.01, fill = Group),outlier.shape = NA, alpha = .5, width = .1, colour = "black")+
+  ylab("Autism polygenic score (PT=0.01)") +
+  xlab("") +
+  ylim(-3,5)+
+  scale_x_discrete(breaks=c("0", "1", "2","3"),labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  geom_point(data = sumPGS_ASD, aes(x = as.numeric(Group)+.1, y = X0.01_mean, group = Group), shape = 18, color="black") +
+  geom_errorbar(data = sumPGS_ASD, aes(x = as.numeric(Group)+.1, y = X0.01_mean, group = Group, ymin = X0.01_mean-se, ymax = X0.01_mean+se), width = .07, color="black")+
+  scale_shape_manual(values=c("0"=1, "1"=13,"2"= 4,"3"=3), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  scale_fill_manual(values=c("0"='springgreen4', "1"='royalblue',"2"= 'darkorange1',"3"='firebrick'), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  scale_color_manual(values=c("0"='springgreen4', "1"='royalblue',"2"= 'darkorange1',"3"='firebrick'), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  coord_flip() +
+  theme_bw() +
+  theme(axis.title.y = element_text(vjust=-1, size=12, hjust=0.5), axis.line = element_line(colour = "black"), text = element_text(size=12),  panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + guides(fill=F, color=F, shape=F)  
+print(rainbowASD_byGroup)
+#dev.off()
+
 
 ##xDx PGS (PT=0.5) by Group 
 
@@ -206,6 +270,7 @@ shapiro.test(ds_prs[ds_prs$Group=='3',]$X0.5)
 
 #ANOVA
 summary(aov(ds_prs$X0.5~as.factor(ds_prs$Group)))
+etaSquared(aov(X0.5~Group, data=ds_prs))
 
 #post-hoc
 TukeyHSD(aov(ds_prs$X0.5~as.factor(ds_prs$Group)))
@@ -239,6 +304,65 @@ xDx_byGroup<-ggplot(ds_prs_plot, aes(x=Group, y=X0.5, colour=Group, fill=Group, 
 print(xDx_byGroup)
 dev.off()
 
+#jpeg(file="rainbowxDx_byGroup.jpeg",width = 5, height = 5, units = 'in', res = 300)
+rainbowxDx_byGroup<-ggplot(ds_prs_plot, aes(x=Group,y=X0.5, fill = Group, colour = Group)) +
+  geom_flat_violin(aes(fill = Group),position = position_nudge(x = .1, y = 0), adjust = 1.5, trim = FALSE, alpha = .5, colour = NA)+
+  geom_point(aes(x = as.numeric(Group)-.15, y = X0.5, colour = Group, shape=Group),position = position_jitter(width = .05), size = 1.5)+
+  geom_boxplot(aes(x = Group, y = X0.5, fill = Group),outlier.shape = NA, alpha = .5, width = .1, colour = "black")+
+  ylab("xDx polygenic score (PT=0.5)") +
+  xlab("") +
+  ylim(-3,5)+
+  scale_x_discrete(breaks=c("0", "1", "2","3"),labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  geom_point(data = sumPGS_ASD, aes(x = as.numeric(Group)+.1, y = X0.01_mean, group = Group), shape = 18, color="black") +
+  geom_errorbar(data = sumPGS_ASD, aes(x = as.numeric(Group)+.1, y = X0.01_mean, group = Group, ymin = X0.01_mean-se, ymax = X0.01_mean+se), width = .07, color="black")+
+  scale_shape_manual(values=c("0"=1, "1"=13,"2"= 4,"3"=3), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  scale_fill_manual(values=c("0"='springgreen4', "1"='royalblue',"2"= 'darkorange1',"3"='firebrick'), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  scale_color_manual(values=c("0"='springgreen4', "1"='royalblue',"2"= 'darkorange1',"3"='firebrick'), name=" ", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
+  coord_flip() +
+  theme_bw() +
+  theme(axis.title.y = element_text(vjust=-1, size=12, hjust=0.5), axis.line = element_line(colour = "black"), text = element_text(size=12),  panel.grid.major = element_blank(), panel.grid.minor = element_blank()) + guides(fill=F, color=F, shape=F)  
+print(rainbowxDx_byGroup)
+dev.off()
+
+
+### MSEL Early Learning composite at 8 months
+
+#check assumptions
+leveneTest(ds_prs$M_ELC_SS_8~ds_prs$Group)
+shapiro.test(ds_prs[ds_prs$Group=='0',]$M_ELC_SS_8)
+shapiro.test(ds_prs[ds_prs$Group=='1',]$M_ELC_SS_8)
+shapiro.test(ds_prs[ds_prs$Group=='2',]$M_ELC_SS_8)
+shapiro.test(ds_prs[ds_prs$Group=='3',]$M_ELC_SS_8)
+
+
+#ANOVA
+summary(aov(M_ELC_SS_8~Group, data=ds_prs))
+etaSquared(aov(M_ELC_SS_8~Group, data=ds_prs))
+
+#post-hoc
+TukeyHSD(aov(M_ELC_SS_8~Group, data=ds_prs))
+
+
+
+### MSEL Early Learning composite at 3 years
+
+#check assumptions
+leveneTest(ds_prs$M_ELC_SS_36~ds_prs$Group)
+shapiro.test(ds_prs[ds_prs$Group=='0',]$M_ELC_SS_36)
+shapiro.test(ds_prs[ds_prs$Group=='1',]$M_ELC_SS_36)
+shapiro.test(ds_prs[ds_prs$Group=='2',]$M_ELC_SS_36)
+shapiro.test(ds_prs[ds_prs$Group=='3',]$M_ELC_SS_36)
+
+
+#ANOVA
+kruskal.test(M_ELC_SS_36~Group, data=ds_prs)
+etaSquared(aov(M_ELC_SS_8~Group, data=ds_prs))
+kruskal_effsize(M_ELC_SS_36~Group, data=ds_prs)
+
+#post-hoc
+dunnTest(M_ELC_SS_36~Group, data=ds_prs)
+
+
 
 ### Regressions
 
@@ -260,9 +384,10 @@ summary(lm(fn_n290_lat~X0.01+ sex + Phase + M_CA_8 + tissue, data=ds_prs))
 
 #jpeg(file="N290_ASDpgs.jpeg",width = 7, height = 5, units = 'in', res = 300)
 N290_ASDpgs<-ggplot(data=ds_prs_plot, aes(x=X0.01, y=fn_n290_lat)) + 
-  ylab("F-N N290 latency (ms)\n Non-face > Face                                                 Face > Non-face") +
+  #ylab("F-N N290 latency (ms)\n Non-face > Face                                                 Face > Non-face") +
+  ylab("") +
   xlab("Autism polygenic score (PT=0.01)") +
-  xlim(-2.1,3.75)+
+  xlim(-2.4,3.75)+
   geom_smooth(method = "lm", se = T, color="black") +
   geom_point(aes(color=as.factor(Group), shape=as.factor(Group))) + 
   scale_color_manual(values=c("0"='springgreen4', "1"='royalblue',"2"= 'darkorange1',"3"='firebrick'), name="", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
@@ -289,9 +414,10 @@ summary(lm(fn_n290_lat~X0.5+ sex + Phase + M_CA_8 + tissue, data=ds_prs))
 
 #jpeg(file="N290_xDxpgs.jpeg",width = 7, height = 5, units = 'in', res = 300)
 N290_xDxpgs<-ggplot(data=ds_prs_plot, aes(x=X0.5, y=fn_n290_lat)) + 
-  ylab("F-N N290 latency (ms)\n Non-face > Face                                                 Face > Non-face") +
+  #ylab("F-N N290 latency (ms)\n Non-face > Face                                                 Face > Non-face") +
+  ylab("") +
   xlab("xDx polygenic score (PT=0.5)") +
-  xlim(-2.1,3.75)+
+  xlim(-2.4,3.75)+
   geom_smooth(method = "lm", se = T, color="black") +
   geom_point(aes(color=as.factor(Group), shape=as.factor(Group))) + 
   scale_color_manual(values=c("0"='springgreen4', "1"='royalblue',"2"= 'darkorange1',"3"='firebrick'), name="", labels=c("noFH", "FH-TD", "FH-Other","FH-Aut")) +
@@ -305,9 +431,19 @@ print(N290_xDxpgs)
 #jpeg('Figure1.jpg', units='in', width=12, height = 6, res = 300)
 Figure1<-ggarrange(N290_ASDpgs, N290_xDxpgs, labels = c("A", "B"),
                    ncol = 2, nrow = 1,
-                   common.legend = TRUE, legend = "bottom")
+                    legend = "bottom")
 print(Figure1)
 #dev.off()
+p<-plot_spacer()
+## Figure 1 for paper
+jpeg('Figure1_2.jpg', units='in', width=18, height = 8, res = 300)
+Figure1_2<-ggarrange(rremove("x.text"), ggarrange(rainbowASD_byGroup, rainbowxDx_byGroup, labels = c("B", "C")),
+                     N290_byGroup, ggarrange(N290_ASDpgs, N290_xDxpgs, common.legend = TRUE, legend = "right" , labels = c("E", "F")),
+                   ncol = 2, widths = c(1, 2), nrow = 2,labels = c("A", "D"), align='hv')
+
+print(Figure1_2)
+dev.off()
+
 
 ## Figure 0 combining barplots
 jpeg('Figure0.jpg', units='in', width=12, height = 6, res = 300)
@@ -316,3 +452,34 @@ Figure0<-ggarrange(ASD_byGroup, xDx_byGroup, labels = c("A", "B"),
                   common.legend = TRUE, legend = "bottom")
 print(Figure0)
 dev.off()
+
+
+
+### test models for prediction of autism
+
+#create case-control binary variable
+ds_prs$CCpheno<-rep("0",nrow(ds_prs))
+ds_prs$CCpheno[ds_prs$Group=="3"]<-"1"
+ds_prs$CCpheno<-factor(ds_prs$CCpheno)
+
+#check polygenic score is different by case-control status
+t.test(ds_prs$X0.01~ds_prs$CCpheno)
+
+
+ModelBaseline <- glm(CCpheno ~ 1, data=ds_prs, family=binomial(link='logit')) #null model
+MB_n290<-update(ModelBaseline, .~. + fn_n290_lat) #add F-N N290 as predictor
+MB_n290_gen<-update(MB_n290, .~. + X0.01) #add PGSaut as predictor
+summary(MB_n290_gen) 
+summary(MB_n290)
+
+#compare model fit
+lrtest(ModelBaseline, MB_n290,MB_n290_gen) 
+BIC(MB_n290,MB_n290_gen)
+
+#obtain McFadden's R2
+pR2(MB_n290_gen)
+pR2(MB_n290)
+
+
+
+
